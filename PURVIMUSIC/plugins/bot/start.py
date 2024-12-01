fbd09dadf088b0a5343d49956ebc055e041f78f9
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-12-01 13:30:40 +0530

    Update start.py

--- PURVIMUSIC/plugins/bot/start.py
+++ PURVIMUSIC/plugins/bot/start.py
@@ -28,7 +28,7 @@ from strings import get_string
 
 #--------------------------
 
-NEXI_VID = [
+NEXI_IMAGE = [
 "https://telegra.ph/file/1a3c152717eb9d2e94dc2.mp4",
 "https://graph.org/file/ba7699c28dab379b518ca.mp4",
 "https://graph.org/file/83ebf52e8bbf138620de7.mp4",
@@ -49,8 +49,8 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_video(
-                random.choice(NEXI_VID),
+            return await message.reply_image(
+                random.choice(NEXI_IMAGE),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -63,7 +63,7 @@ async def start_pm(client, message: Message, _):
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_text("Love U Baby")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
@@ -101,8 +101,8 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_video(
-            random.choice(NEXI_VID),
+        await message.reply_image(
+            random.choice(NEXI_IMAGE),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -118,8 +118,8 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_video(
-        random.choice(NEXI_VID),
+    await message.reply_image(
+        random.choice(NEXI_IMAGE),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -153,8 +153,8 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-                await message.reply_video(
-                    random.choice(NEXI_VID),
+                await message.reply_image(
+                    random.choice(NEXI_IMAGE),
                     caption=_["start_3"].format(
                         message.from_user.mention,
                         app.mention,
